381ab860ea26680446aca69c08b97a85017dedbd
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-15 13:21:52 +0300

    [OTHER] spec_helper.rb updated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,7 @@
 require "hexx-suit"
 
 # Loads runtime metrics in the current scope
-Hexx::Suit.load_metrics_for(self) if ENV["USE_METRICS"]
-
-# Loads the RSpec support files.
-Dir.chdir File.expand_path("..", __FILE__) do
-  Dir["./support/config/*.rb"].each { |file| require file }
-end
+Hexx::Suit.load_metrics_for(self)
 
 # Loads the code of the module.
 require "hexx-validators"
